681e53d65345d03942c2229b751094a9cd72408e
Manish Bhardwaj <manishbhardwaj131149@gmail.com>
2023-11-17 22:20:35 +0530

    secont commit

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,7 @@
 const express = require('express');
 const app = express();
+
+app.get('/', (req, res) => {
+    res.send("I M BACK")
+})
 app.listen(5000, console.log("server running on port 5000"))
